77277a84c8351d51e4e553015331ae85377d0b05
TaufanFadhilah <tfi231097@gmail.com>
2018-02-16 19:01:13 +0700

    Recyler View onClick done

--- app/src/main/java/com/example/android/taufan_fadhilah_iskandar_1202150017_modul2/MenuAdapter.java
+++ app/src/main/java/com/example/android/taufan_fadhilah_iskandar_1202150017_modul2/MenuAdapter.java
@@ -1,12 +1,15 @@
 package com.example.android.taufan_fadhilah_iskandar_1202150017_modul2;
 
 import android.content.Context;
+import android.content.Intent;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.LinkedList;
 
@@ -50,18 +53,39 @@ public class MenuAdapter extends
         return mFoodList.size();
     }
 
-    class MenuViewHolder extends RecyclerView.ViewHolder {
+    class MenuViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
         public final TextView foodItemView;
         public final TextView priceItemView;
         public final ImageView photoItemView;
 
         final MenuAdapter mAdapter;
-        public MenuViewHolder(View itemView, MenuAdapter adapter) {
+        public MenuViewHolder(final View itemView, MenuAdapter adapter) {
             super(itemView);
             foodItemView = (TextView)itemView.findViewById(R.id.tv_food);
             priceItemView = (TextView)itemView.findViewById(R.id.tv_price);
             photoItemView = (ImageView)itemView.findViewById(R.id.iv_photo);
+            photoItemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    // Get the position of the item that was clicked.
+                    int mPosition = getLayoutPosition();
+                    // Use that to access the affected item in mWordList.
+                    String food = mFoodList.get(mPosition);
+                    Integer price = mPriceList.get(mPosition);
+                    Integer photo = mPriceList.get(mPosition);
+                    Intent intent = new Intent(itemView.getContext(),DetailMenu.class);
+                    intent.putExtra("food",food);
+                    intent.putExtra("price",price);
+                    intent.putExtra("photo",photo);
+                    itemView.getContext().startActivity(intent);
+                }
+            });
             this.mAdapter = adapter;
+        }
+
+        @Override
+        public void onClick(View view) {
+
         }
     }
 }
